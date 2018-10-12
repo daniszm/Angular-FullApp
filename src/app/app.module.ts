8403cb99dc1affeed1f7358d515c6450e2f35245
Danisz Mydlarz <danisz.mydlarz@pl.abb.com>
2018-10-12 11:24:44 +0200

    Add flex-layout to the project. Add form to signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './material.module';
@@ -29,6 +30,8 @@ import { AppRoutingModule } from './app-routing.module';
     LoginComponent
   ],
   imports: [
+    FormsModule,
+    FlexLayoutModule,
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
