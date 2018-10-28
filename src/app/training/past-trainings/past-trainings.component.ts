a1b44f19a690c99f0abd5f68875aa0480d71d264
daniszm <daniszek9933@gmail.com>
2018-10-28 14:52:42 +0100

    Add sorting options to the past trainings table

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,22 +1,28 @@
 import { TrainingService } from './../training.service';
 import { Exercise } from './../excercise.model';
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-past-trainings',
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.css']
 })
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
 
-  displayedColumns = ["date", "name", "duration", "calories", "state"];
+  displayedColumns = ["name", "date", "duration", "calories", "state"];
   dataSource = new MatTableDataSource<Exercise>();
+
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.getCompletedOrCanceledExercises();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
 }
