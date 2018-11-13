1e7058315859c3c705d70cdec70edd31bd17ffb5
Danisz Mydlarz <danisz.mydlarz@pl.abb.com>
2018-11-13 12:10:36 +0100

    Connect data table to firestore (Past Excercises)

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,7 +1,8 @@
 import { TrainingService } from './../training.service';
 import { Exercise } from './../excercise.model';
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-past-trainings',
@@ -12,6 +13,7 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
 
   displayedColumns = ["name", "date", "duration", "calories", "state"];
   dataSource = new MatTableDataSource<Exercise>();
+  private exChangedSubscription: Subscription;
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -19,12 +21,20 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
   constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
-    this.dataSource.data = this.trainingService.getCompletedOrCanceledExercises();
+    this.exChangedSubscription = this.trainingService.finishedExercisesChanged
+    .subscribe((exercises: Exercise[]) => {
+      this.dataSource.data = exercises;
+    });
+    this.trainingService.fetchCompletedOrCanceledExercises();
   }
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnDestroy() {
+    this.exChangedSubscription.unsubscribe();
   }
 
   doFilter(filterValue: string) {
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -8,13 +8,13 @@ import { map } from 'rxjs/operators';
 export class TrainingService {
     exerciseChanged = new Subject<Exercise>();
     exercisesChanged = new Subject<Exercise[]>();
+    finishedExercisesChanged = new Subject<Exercise[]>();
 
     name: string;
 
     private availableExercises: Exercise[] = [];
 
     private runningExercise: Exercise;
-    private exercises: Exercise[] = [];
 
     constructor(private db: AngularFirestore) { }
 
@@ -39,7 +39,7 @@ export class TrainingService {
     }
 
     completeExercise() {
-        this.exercises.push({
+        this.addDataToDatabase({
             ...this.runningExercise,
             date: new Date(),
             state: 'completed'
@@ -49,7 +49,7 @@ export class TrainingService {
     }
 
     cancelExercise(progress: number) {
-        this.exercises.push({
+        this.addDataToDatabase({
             ...this.runningExercise,
             duration: this.runningExercise.duration * (progress / 100),
             calories: this.runningExercise.calories * (progress / 100),
@@ -70,7 +70,15 @@ export class TrainingService {
         return { ...this.runningExercise }
     }
 
-    getCompletedOrCanceledExercises() {
-        return this.exercises.slice();
+    fetchCompletedOrCanceledExercises() {
+        this.db.collection('finishedExercises')
+            .valueChanges()
+            .subscribe((exercises: Exercise[]) => {
+                this.finishedExercisesChanged.next(exercises);
+            });
+    }
+
+    private addDataToDatabase(exercise: Exercise) {
+        this.db.collection('finishedExercises').add(exercise);
     }
 }
