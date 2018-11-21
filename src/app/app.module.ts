26ab613d8a1d46fd05b7df11ce7bd6440530833a
Danisz Mydlarz <danisz.mydlarz@pl.abb.com>
2018-11-21 16:48:16 +0100

    Split routing module into 2 files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './material.module';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { AuthService } from './auth/auth.service';
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,6 +1,4 @@
 import { AuthGuard } from './auth/auth.guard';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from "@angular/core";
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -8,8 +6,6 @@ import { TrainingComponent } from './training/training.component';
 
 const routes: Routes = [
     {path: '', component: WelcomeComponent},
-    {path: 'signup', component: SignupComponent},
-    {path: 'login', component: LoginComponent},
     {path: 'training', component: TrainingComponent, canActivate: [AuthGuard]}
 ];
 
