c687b52d8469e3f7119d798e7d9f547f9e179156
Danisz Mydlarz <danisz.mydlarz@pl.abb.com>
2018-11-23 11:55:53 +0100

    Update environment.prod file to deploy an app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCnF2CDvqRH_6KaZ4we8-AQbeLIFMwTCw4",
+    authDomain: "ng-fitness-tracker-ada88.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-ada88.firebaseio.com",
+    projectId: "ng-fitness-tracker-ada88",
+    storageBucket: "ng-fitness-tracker-ada88.appspot.com",
+    messagingSenderId: "101623432674"
+  }
 };
